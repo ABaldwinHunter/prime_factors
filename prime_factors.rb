b4b58a576672953b63f09afb16a6889c9ed3e5b4
Ashley Baldwin-Hunter <ashley.baldwinhunter@gmail.com>
2015-04-28 23:11:26 -0400

    creates method for returning array of prime factors

--- prime_factors.rb
+++ prime_factors.rb
@@ -1,20 +1,29 @@
-def prime_factors(num)
-  factors = []
-  if num < 1
-    return []
-  elsif num == 2
-    return [2]
+require 'pry'
+
+def prime_factors(num, prime_factors=[])
+  if num <= 1
+    return prime_factors
+  elsif isPrime?(num)
+    prime_factors << num
   else
-    n = 2
-    while num < Math.sqrt(num)
-      if num % n == 0
-        factors << n
-        factors << prime_factors(num/n)
-        return factors
+    i = 2
+    while i <= Math.sqrt(num)
+      # binding.pry
+      if num % i == 0
+        prime_factors << i
+        return prime_factors(num/i, prime_factors)
       end
-      n +=1
+      i +=1
     end
   end
+  return prime_factors
+end
+
+def isPrime?(num)
+  for i in 2..num-1
+    return false if num % i == 0
+  end
+  true
 end
 
 num = ARGV[0].to_i
